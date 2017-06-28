afaf23b4aceb5bbb263238a5417ea0d47cd624cd
Brandon Tang <blt264@nyu.edu>
2017-06-28 10:43:31 -0700

    Changed repoflag to force rescan of all repos

--- modules/packages/manifests/setup.pp
+++ modules/packages/manifests/setup.pp
@@ -169,7 +169,7 @@ class packages::setup {
 
             # to flush the metadata cache, increase this value by one (or
             # anything, really, just change it).
-            $repoflag = 85
+            $repoflag = 167
             file {
                 '/etc/.repo-flag':
                     content =>
