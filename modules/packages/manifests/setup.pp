40bd2f4ce2e51a1e8bf6021432166049a1751b0b
Brandon Tang <blt264@nyu.edu>
2017-06-28 14:10:34 -0700

    Changed repoflag number

--- modules/packages/manifests/setup.pp
+++ modules/packages/manifests/setup.pp
@@ -169,7 +169,7 @@ class packages::setup {
 
             # to flush the metadata cache, increase this value by one (or
             # anything, really, just change it).
-            $repoflag = 167
+            $repoflag = 168
             file {
                 '/etc/.repo-flag':
                     content =>
