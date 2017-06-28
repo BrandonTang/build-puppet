76e3c6ef6cf86ae14e8876d29c31cbe37f312db7
Brandon Tang <blt264@nyu.edu>
2017-06-28 11:09:38 -0700

    Changed ensure to 0.3.1-1 instead of latest

--- modules/packages/manifests/mozilla/lego.pp
+++ modules/packages/manifests/mozilla/lego.pp
@@ -1,13 +1,12 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
-
 class packages::mozilla::lego {
     case $::operatingsystem {
         CentOS: {
             package {
                 'lego':
-                    ensure => latest;
+                    ensure => '0.3.1-1';
             }
         }
         default: {
