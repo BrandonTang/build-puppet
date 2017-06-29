184994c681480558cddc641b91f418a299f93630
Brandon Tang <blt264@nyu.edu>
2017-06-29 09:27:47 -0700

    Removed realize command for testing purposes

--- modules/packages/manifests/mozilla/lego.pp
+++ modules/packages/manifests/mozilla/lego.pp
@@ -3,7 +3,6 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 class packages::mozilla::lego {
-    realize(Packages::Yumrepo['lego'])
     case $::operatingsystem {
         CentOS: {
             package {
